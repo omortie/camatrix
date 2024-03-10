1a6f26705daa9ac04b28eba7fe28f1bac194db06
Morteza Sherafati <sherafati.morteza@gmail.com>
2024-03-10 11:02:56 +0000

    added a save load settings for any required setting feature

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  double aspectRatio = 0.8;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSettings();
+  }
+
+  Future<void> _loadSettings() async {
+    final prefs = await SharedPreferences.getInstance();
+    aspectRatio = prefs.getDouble("aspectRatio") ?? 0.8;
+    setState(() {});
+  }
+
+  Future<void> _saveSettings() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setDouble("aspectRatio", aspectRatio);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("Dummy Settings"),
+      child: Column(
+        children: [
+          Text("Dummy Settings"),
+          Slider(
+            label: "Aspect Ratio",
+            value: aspectRatio,
+            onChanged: (value) {
+              setState(() {
+                aspectRatio = value;
+              });
+              _saveSettings();
+            },
+          )
+        ],
+      ),
     );
   }
 }
