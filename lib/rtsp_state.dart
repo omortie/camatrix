bc09eaaa16a35669c5356f1567db1f557ccd83e7
Morteza Sherafati <sherafati.morteza@gmail.com>
2024-03-18 13:55:30 +0000

    add some simple fetch command to serialize rtsps

--- lib/rtsp_state.dart
+++ lib/rtsp_state.dart
@@ -1,5 +1,6 @@
 import 'dart:collection';
-
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 class RTSP {
@@ -9,10 +10,21 @@ class RTSP {
   final String name;
   final int frameRate;
 
-  RTSP.fromJson(Map<String, dynamic> json)
-      : url = json['url'] as String,
-        name = json['name'] as String,
-        frameRate = json['frameRate'] as int;
+  factory RTSP.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+        'title': String name,
+        'userId': int frameRate,
+      } =>
+        RTSP(
+          name: name,
+          url:
+              'rtsp://rtspstream:a349b013a371642450e3ace0d41b7a9a@zephyr.rtsp.stream/pattern',
+          frameRate: frameRate,
+        ),
+      _ => throw const FormatException('failed to load rtsp'),
+    };
+  }
 
   Map<String, dynamic> toJson() => {
         'url': url,
@@ -36,5 +48,20 @@ class RTSPState extends ChangeNotifier {
     _rtspList.clear();
 
     notifyListeners();
+  }
+
+  Future<void> fetchRTSP() async {
+    final response = await http
+        .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      add(RTSP.fromJson(jsonDecode(response.body) as Map<String, dynamic>));
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load rtsp');
+    }
   }
 }
