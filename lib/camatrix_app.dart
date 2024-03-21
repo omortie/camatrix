e5c4e5e8a8d0c856704c4c48a00dcd888c7a85fa
Morteza Sherafati <sherafati.morteza@gmail.com>
2024-03-21 19:53:01 +0000

    added multiple window communication API test

--- lib/camatrix_app.dart
+++ lib/camatrix_app.dart
@@ -1,7 +1,31 @@
-import 'package:camatrix/home_widget.dart';
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class CamatrixApp extends StatelessWidget {
+import 'package:camatrix/home_widget.dart';
+import 'package:camatrix/rtsp_state.dart';
+import 'package:desktop_multi_window/desktop_multi_window.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
+class CamatrixApp extends StatefulWidget {
+  @override
+  State<CamatrixApp> createState() => _CamatrixAppState();
+}
+
+class _CamatrixAppState extends State<CamatrixApp> {
+  @override
+  void initState() {
+    super.initState();
+
+    DesktopMultiWindow.setMethodHandler(_handleMethodCallback);
+  }
+
+  @override
+  dispose() {
+    super.dispose();
+    DesktopMultiWindow.setMethodHandler(null);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,5 +36,20 @@ class CamatrixApp extends StatelessWidget {
       ),
       home: HomeWidget(),
     );
+  }
+
+  Future<dynamic> _handleMethodCallback(
+      MethodCall call, int fromWindowId) async {
+    if (call.arguments.toString() == "ping") {
+      return "pong";
+    }
+
+    if (call.method == "setRTSP") {
+      final rtspReceived = (jsonDecode(call.arguments) as List)
+          .map((e) => e as Map<String, dynamic>)
+          .toList();
+      Future.microtask(
+          () => context.read<RTSPState>().setRTSPList(rtspReceived));
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:camatrix/camatrix.dart';
+import 'package:desktop_multi_window/desktop_multi_window.dart';
 import 'package:flutter/material.dart';
 
 const mockRTSPData = [
@@ -12,38 +15,68 @@ const mockRTSPData = [
     'url': 'rtsp://admin:camteam524@31.154.52.236:10500',
     'frameRate': 20,
   },
-  {
-    'name': 'Camera 3',
-    'url': 'rtsp://admin:camteam524@31.154.52.236:10500',
-    'frameRate': 20,
-  },
-  {
-    'name': 'Camera 4',
-    'url': 'rtsp://admin:camteam524@31.154.52.236:10500',
-    'frameRate': 20,
-  },
-  {
-    'name': 'Camera 5',
-    'url': 'rtsp://admin:camteam524@31.154.52.236:10500',
-    'frameRate': 20,
-  },
-  {
-    'name': 'Camera 6',
-    'url': 'rtsp://admin:camteam524@31.154.52.236:10500',
-    'frameRate': 20,
-  },
-  {
-    'name': 'Camera 7',
-    'url': 'rtsp://admin:camteam524@31.154.52.236:10500',
-    'frameRate': 20,
-  },
-  {
-    'name': 'Camera 8',
-    'url': 'rtsp://admin:camteam524@31.154.52.236:10500',
-    'frameRate': 20,
-  },
 ];
 
-void main() {
-  runApp(Camatrix(rtspData: mockRTSPData));
+void main(List<String> args) {
+  if (args.firstOrNull == 'multi_window') {
+    final windowId = int.parse(args[1]);
+
+    runApp(Camatrix());
+  } else {
+    // runApp(Camatrix(rtspData: mockRTSPData));
+    runApp(MyApp());
+  }
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late int slaveWindowId;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "MainApp",
+      home: Container(
+        color: Colors.cyan,
+        child: Center(
+          child: Card(
+            clipBehavior: Clip.antiAlias,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () async {
+                    final window = await DesktopMultiWindow.createWindow(
+                        jsonEncode(mockRTSPData));
+                    window
+                      ..center()
+                      ..setTitle('Camatrix')
+                      ..show();
+                    setState(() {
+                      slaveWindowId = window.windowId;
+                    });
+                  },
+                  child: const Text("Open Camatrix"),
+                ),
+                TextButton(
+                  onPressed: () => addRTSPToCamatrix(),
+                  child: const Text("Add RTSP"),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void addRTSPToCamatrix() async {
+    final result = await DesktopMultiWindow.invokeMethod(
+        slaveWindowId, "setRTSP", jsonEncode(mockRTSPData));
+    debugPrint("result: $result");
+  }
 }
