311da93afc4f4d43242931daebac80f7a7628d43
Morteza Sherafati <sherafati.morteza@gmail.com>
2024-03-27 10:49:29 +0000

    made camatrix app stateless

--- lib/camatrix_app.dart
+++ lib/camatrix_app.dart
@@ -1,15 +1,7 @@
 import 'package:camatrix/home_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:logger/logger.dart';
 
-class CamatrixApp extends StatefulWidget {
-  @override
-  State<CamatrixApp> createState() => _CamatrixAppState();
-}
-
-class _CamatrixAppState extends State<CamatrixApp> {
-  var logger = Logger();
-
+class CamatrixApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
